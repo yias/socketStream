15492222d0244fe5b6022abebd1dc16d4d38ba62
= <=>
2020-03-01 20:45:05 +0100

    fist implementation complete; cpp client, python server

---
+++ pyTCP/tcp_server.py
@@ -0,0 +1,205 @@
+#!/usr/bin/env python
+"""
+	developer: Iason Batzianoulis
+	maintaner: Iason Batzianoulis
+	email: iasonbatz@gmail.com
+	description: 
+	This scripts creates a TCP/IP server for listening to inputs from a space navigator
+"""
+
+
+# import standard modules 
+import numpy as np
+import argparse
+import hashlib
+import random
+import string
+
+# import modules for socket programming (TCP/IP connection)
+import socket
+import sys
+import json
+
+# import modules for theading programming
+import threading
+import time
+
+
+def calc_checksum(s):
+    return '%2X' % (-(sum(ord(c) for c in s) % 256) & 0xFF)
+
+def msgExtractor(msg, hdrSize, endMsgID):
+    msgSize=int(msg[:hdrSize].decode('utf-8'))
+    tmp_msg=msg[hdrSize:hdrSize+msgSize-sys.getsizeof('')]
+    hashcode=msg[hdrSize+msgSize-sys.getsizeof(''):-len(endMsgID)]
+    hc_check=hashlib.md5()
+    hc_check.update(tmp_msg)
+    if hc_check.hexdigest()==hashcode:
+        return True, tmp_msg
+    else:
+        return False, ''
+
+
+def randomString(strlength=10):
+	letters=string.ascii_lowercase
+	return ''.join(random.choice(letters) for i in range(strlength))
+
+
+def handShake(conn, strlength):
+
+	ping_times=np.empty([10,1], dtype=np.float64)
+	compute_times=np.empty([10,1], dtype=np.float64)
+	validity_counter=np.empty([10,1], dtype=bool)
+	endMSG="!3tt"
+	HEADERSIZE=4
+	t_time0=0.0
+
+	# receive and send random msgs to the client 10 times
+	for i in range(10):
+		msg_full=''
+		while (True):
+			dataT=conn.recv(4)
+			msg_full+=dataT
+			if msg_full[-4:].decode('utf-8')==endMSG:
+				break
+
+		if t_time0!=0.0:
+			ping_times[i-1]=time.time()-t_time0
+		t_time=time.time()
+		msg_validity, tr_msg = msgExtractor(msg_full,HEADERSIZE,endMSG)
+		validity_counter[i]=msg_validity
+
+		dcdr=hashlib.md5()
+		test_msg=randomString(strlength)
+		dcdr.update(test_msg)
+		chSum=dcdr.hexdigest()
+		msg_len=('{:<'+str(HEADERSIZE)+'}').format(str(sys.getsizeof(test_msg)))
+		compute_times[i]=time.time()-t_time
+		conn.sendall(msg_len.encode('utf-8')+(test_msg).encode('utf-8')+chSum.encode('utf-8')+endMSG.encode('utf-8'))
+		t_time0=time.time()
+
+	if((1*(validity_counter)).mean()>0.8):
+		print('valid communication established')
+		print('compute times: %s %s %s s' %(compute_times.mean(), u'\u00b1', compute_times.std()))
+		print('ping times:  %s %s %s s' %(ping_times[:9].mean(), u'\u00b1', ping_times[:9].std()))
+		return True
+	else:
+		print('communication is not valid')
+		return False
+
+
+
+def main(args):
+
+	# Create a TCP/IP socket
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+	# Bind the socket to the port
+	server_address = (args.host, args.port)
+
+	print('starting up on %s port %s' % server_address)
+
+	sock.bind(server_address)
+
+	# initialize client and adresses lists
+	# connections_list=list() #np.empty([1,])
+	# clients_address=list() #np.empty([1,])
+
+	# define buffer size
+	BUFFER_SIZE=4
+
+	# define a header size of the message
+	HEADERSIZE=8
+
+	# define message identifier
+	msg_idf="!&?5"
+
+	# end-of-message identifier
+	endMSG="!3tt"
+
+	# end-connection identifier
+	ec_id="\ne@c"
+	# listen for new connections
+	# sock.settimeout(3)
+	sock.listen(6)
+	#Wait for a connection
+	connection_exist=False
+	print('waiting for a connections ... ')
+
+	while(True):
+		try:
+			# check if any connection inquire exists and accept it
+			connection, client_address = sock.accept()
+			connection_exist=True
+			print('connection from ', client_address)
+
+			# check communication validity with a hand-shake protocol
+			# conCheck=handShake(connection,10)
+			while(True):
+			# retrieve the message identifier. once it is received, compose the message
+				data=connection.recv(BUFFER_SIZE)
+				# print(data)
+				# time.sleep(3)
+
+				if data.decode('utf-8')==msg_idf:
+					# print('understood')
+					# receive bytes until the full message is received
+					full_msg=''
+					while (True):
+						dataT=connection.recv(1)
+						full_msg+=dataT.decode("utf-8") 
+						if full_msg[-4:]==endMSG:
+							break
+
+				# extract message
+				# 	msg_validity, tr_msg = msgExtractor(full_msg,HEADERSIZE,endMSG)
+					msg_data=json.loads(full_msg[:-4])
+					print(msg_data)
+					print('name: %s' %(msg_data.get("name")))
+					print('age: %s' %(msg_data.get("age")))
+
+				# 	if msg_validity:
+				# 		# if the message is valid, retrieve the data and load them into a json object
+				# 		msg_data=json.loads(tr_msg.decode('utf-8'))
+
+				# 		print('translation: %s' %(msg_data.get("translation")))
+				# 		print('rotation: %s' %(msg_data.get("rotation")))
+				# 		print('button: %s' %(msg_data.get("button")))
+
+				if  data.decode('utf-8')==ec_id:
+					# if end-of-communication identifier received, terminate the connection
+					print('Connection terminated by client ', client_address)
+					connection.close()
+					break
+            	
+		except KeyboardInterrupt:
+			# if Ctrl+C is pressed in the keyboard, close the connections (if any) and exit
+			if connection_exist:
+				connection.close()
+			break
+		finally:
+			print('Waiting for new clients ....')
+    
+	# close communications
+	sock.close()
+	print('all connections killed')
+
+
+		
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='TCP server for receiving inputs from 3D mouse client')
+
+    parser.add_argument('--host', type=str, help= 'the IP of the server', default='localhost')
+
+    parser.add_argument('--port', type=int, help= 'the port on which the server is listening', default=10352)
+
+    parser.add_argument('--roosnode','--rn', type=bool, help= 'if needed to publish the data to a ros topic', default=False)
+
+    parser.add_argument('--version', '-V', help='show program version', action='store_true')
+
+    args=parser.parse_args()
+
+    main(args)	
--- test_json.cpp
+++ test_json.cpp
@@ -1,27 +1,127 @@
+#define WIN32_LEAN_AND_MEAN
 
 #include <iostream>
 #include <vector>
 #include <string>
 
-#include "rapidjson/writer.h"
+// include library for socket programming
+#include <winsock2.h>
+#include <windows.h>
+
+#include <ws2tcpip.h>
+
+
+// Need to link with Ws2_32.lib, Mswsock.lib, and Advapi32.lib
+#pragma comment (lib, "Ws2_32.lib")
+#pragma comment (lib, "Mswsock.lib")
+#pragma comment (lib, "AdvApi32.lib")
+
+
 #include "rapidjson/document.h"
+#include "rapidjson/writer.h"
 #include "rapidjson/stringbuffer.h"
 
+#define DEFAULT_BUFLEN 512
+#define DEFAULT_PORT "10352"
+#define HOST_IP "192.168.1.104"
 
 
-int main(){
+
+
+int main(int argc, char **argv){
+
+    WSADATA wsaData;
+    SOCKET ConnectSocket = INVALID_SOCKET;
+    struct addrinfo *result = NULL,
+                    *ptr = NULL,
+                    hints;
+    const char *sendbuf = "this is a test";
+
+    memset(&hints, 0, sizeof hints);
+
+    char recvbuf[DEFAULT_BUFLEN];
+    int iResult;
+    int recvbuflen = DEFAULT_BUFLEN;
+    
+    // Validate the parameters
+    if (argc != 2) {
+        printf("usage: %s server-name\n", argv[0]);
+        return 1;
+    }    
+
+    // Initialize Winsock
+    iResult = WSAStartup(MAKEWORD(2,2), &wsaData);
+    if (iResult != 0) {
+        printf("WSAStartup failed with error: %d\n", iResult);
+        return 1;
+    }
+
+    std::cout<<argv[1]<<"\n";
+
+    // Resolve the server address and port
+    iResult = getaddrinfo(argv[1], DEFAULT_PORT, &hints, &result);
+    if ( iResult != 0 ) {
+        printf("getaddrinfo failed with error: %d\n", iResult);
+        WSACleanup();
+        return 1;
+    }
+
+    ZeroMemory( &hints, sizeof(hints) );
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPPROTO_TCP;
+
+    // Attempt to connect to an address until one succeeds
+    for(ptr=result; ptr != NULL ;ptr=ptr->ai_next) {
+
+        // Create a SOCKET for connecting to server
+        ConnectSocket = socket(ptr->ai_family, ptr->ai_socktype, 
+            ptr->ai_protocol);
+        if (ConnectSocket == INVALID_SOCKET) {
+            printf("socket failed with error: %ld\n", WSAGetLastError());
+            WSACleanup();
+            return 1;
+        }
+
+        // Connect to server.
+        iResult = connect( ConnectSocket, ptr->ai_addr, (int)ptr->ai_addrlen);
+        if (iResult == SOCKET_ERROR) {
+            closesocket(ConnectSocket);
+            ConnectSocket = INVALID_SOCKET;
+            continue;
+        }
+        break;
+    }
+
+    freeaddrinfo(result);
+
+    if (ConnectSocket == INVALID_SOCKET) {
+        printf("Unable to connect to server!\n");
+        WSACleanup();
+        return 1;
+    }
+
+    //  define message identifier
+	const char* msg_idf="!&?5";
+
+    //  end-of-message identifier
+	const char* endMSG="!3tt";
+
+    //  end-connection identifier
+	const char* ec_id="\ne@c";
+
 
     const char* json_object ="{\"name\":\"Ted\",\"age\":15}";
 
     rapidjson::Document dDoc;
     dDoc.Parse(json_object);
 
-    std::vector < std::string > t_msg{"test1", "test2"};
+    // std::vector < std::string > t_msg{"test1", "test2"};
     
-    for (const std::string& word : t_msg){
-        std::cout << word << " ";
-    }
-    std::cout<<std::endl;
+    // for (const std::string& word : t_msg){
+    //     std::cout << word << " ";
+    // }
+    // std::cout<<std::endl;
 
     // stringify the jason object
     rapidjson::StringBuffer buffer;
@@ -29,6 +129,76 @@ int main(){
     dDoc.Accept(writer);
 
     std::cout << buffer.GetString() << std::endl;
+    std::cout<<"sending message beginning id ..."<<std::endl;
+    // Send an initial buffer
+    iResult = send( ConnectSocket, msg_idf, (int)strlen(msg_idf), 0 );
+    if (iResult == SOCKET_ERROR) {
+        printf("send failed with error: %d\n", WSAGetLastError());
+        closesocket(ConnectSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    std::cout<<"message-id sent"<<std::endl;
+
+    printf("Bytes Sent: %ld\n", iResult);
+
+    std::cout<<"sending message ..."<<std::endl;
+    const char* t_msg = buffer.GetString();
+    iResult = send( ConnectSocket, t_msg, (int)strlen(t_msg), 0 );
+    if (iResult == SOCKET_ERROR) {
+        printf("send failed with error: %d\n", WSAGetLastError());
+        closesocket(ConnectSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    std::cout<<"message sent"<<std::endl;
+
+    printf("Bytes Sent: %ld\n", iResult);
+
+    std::cout<<"sending end-of-message ..."<<std::endl;
+    iResult = send( ConnectSocket, endMSG, (int)strlen(endMSG), 0 );
+    if (iResult == SOCKET_ERROR) {
+        printf("send failed with error: %d\n", WSAGetLastError());
+        closesocket(ConnectSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    std::cout<<"end-of-message sent"<<std::endl;
+
+    printf("Bytes Sent: %ld\n", iResult);
+
+    std::cout<<"sending end-connection ..."<<std::endl;
+
+    iResult = send( ConnectSocket, ec_id, (int)strlen(ec_id), 0 );
+    if (iResult == SOCKET_ERROR) {
+        printf("send failed with error: %d\n", WSAGetLastError());
+        closesocket(ConnectSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    std::cout<<"end-connection sent"<<std::endl;
+
+
+     // shutdown the connection since no more data will be sent
+    iResult = shutdown(ConnectSocket, SD_SEND);
+    if (iResult == SOCKET_ERROR) {
+        printf("shutdown failed with error: %d\n", WSAGetLastError());
+        closesocket(ConnectSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    
+    printf("Bytes Sent: %ld\n", iResult);
+
+
+    // cleanup
+    closesocket(ConnectSocket);
+    WSACleanup();
 
     return 0;
 }
