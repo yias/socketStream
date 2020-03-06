ec40d6946f15aae35deee7d5377da1d423cd0cab
yias <iasonbatz@gmail.com>
2020-03-06 10:57:41 +0100

    adding timers to count the operation time of the message update and send

--- source/source.cpp
+++ source/source.cpp
@@ -1,7 +1,10 @@
 
 
 #include "socketStream.h"
-#include <algorithm>
+// #include <algorithm>
+#include <chrono>
+// #include <ctime>
+
 
 int main(int argc, char **argv){
 
@@ -21,14 +24,23 @@ int main(int argc, char **argv){
     test_rjson.push_back("age");
     test_rjson.push_back("year");
 
+    auto start = std::chrono::steady_clock::now();
+
     if(socketHdlr.initialize_msgStruct(test_rjson)<0){
         std::cerr << "Unable to inizialize message structure" << std::endl;
         return -1;
     }
 
+    auto end = std::chrono::steady_clock::now();
+    std::cout << "Message initialization: " << (double)(std::chrono::duration_cast<std::chrono::microseconds>(end-start).count())/1000.0 << " ms" << std::endl;
+
     std::string sfield("name");
     char *svalue = {"Iason"};
+
+    start = std::chrono::steady_clock::now();
     socketHdlr.updateMSG(sfield, svalue);
+    end = std::chrono::steady_clock::now();
+    std::cout << "Message update (filed names): " << (double)std::chrono::duration_cast<std::chrono::microseconds>(end-start).count()/1000.0 << " ms" << std::endl;
 
 
     sfield="year";
@@ -37,7 +49,11 @@ int main(int argc, char **argv){
     std::vector< std::vector<double> > t_value(2);
     t_value[0]=std::vector<double>(t_value1, t_value1 +(sizeof(t_value1)/sizeof(t_value1[0])));
     t_value[1]=std::vector<double>(t_value2, t_value2 +(sizeof(t_value2)/sizeof(t_value2[0])));
+    
+    start = std::chrono::steady_clock::now();
     socketHdlr.updateMSG(sfield, t_value);
+    end = std::chrono::steady_clock::now();
+    std::cout << "Message update (array of arrays): " << (double)std::chrono::duration_cast<std::chrono::microseconds>(end-start).count()/1000.0 << " ms" << std::endl;
      
 
     if(socketHdlr.initialize_sockeStream()<0){
@@ -49,34 +65,63 @@ int main(int argc, char **argv){
         std::cerr << "Unable to connect to " << srvIP << std::endl;
         return -1;
     }
-
+    start = std::chrono::steady_clock::now();
     if(socketHdlr.sendMSg()<0){
         std::cerr << "unable to send message " << std::endl;
         return -1;
     }
+    end = std::chrono::steady_clock::now();
+    std::cout << "Sending message: " << (double)std::chrono::duration_cast<std::chrono::microseconds>(end-start).count()/1000.0 << " ms" << std::endl;
 
-    std::cout << "the sent message: " << socketHdlr.getFullmsg() << std::endl;
+    // std::cout << "the sent message: " << socketHdlr.getFullmsg() << std::endl;
 
     t_value[0][1]=45.4;
     t_value[1][2]=1111.456;
+    start = std::chrono::steady_clock::now();
     socketHdlr.updateMSG(sfield, t_value);
     if(socketHdlr.sendMSg()<0){
         std::cerr << "unable to send message " << std::endl;
         return -1;
     }
+    end = std::chrono::steady_clock::now();
+    std::cout << "Update and send message: " << (double)std::chrono::duration_cast<std::chrono::microseconds>(end-start).count()/1000.0 << " ms" << std::endl;
 
-    std::cout << "the sent message: " << socketHdlr.getFullmsg() << std::endl;
+    start = std::chrono::steady_clock::now();
+    socketHdlr.updateMSG(sfield, t_value);
+    if(socketHdlr.sendMSg()<0){
+        std::cerr << "unable to send message " << std::endl;
+        return -1;
+    }
+    end = std::chrono::steady_clock::now();
+    std::cout << "Update and send message 2: " << (double)std::chrono::duration_cast<std::chrono::microseconds>(end-start).count()/1000.0 << " ms" << std::endl;
+
+    // std::cout << "the sent message: " << socketHdlr.getFullmsg() << std::endl;
 
     t_value[0][1]=12.6874;
     t_value[1][2]=222.5609;
-    socketHdlr.updateMSG(sfield, t_value);
     socketHdlr.setHashKey(false);
+
+    start = std::chrono::steady_clock::now();
+    socketHdlr.updateMSG(sfield, t_value);
+    
     if(socketHdlr.sendMSg()<0){
         std::cerr << "unable to send message " << std::endl;
         return -1;
     }
+    end = std::chrono::steady_clock::now();
+    std::cout << "Update and send message (without checksum): " << (double)std::chrono::duration_cast<std::chrono::microseconds>(end-start).count()/1000.0 << " ms" << std::endl;
 
-    std::cout << "the sent message: " << socketHdlr.getFullmsg() << std::endl;
+    start = std::chrono::steady_clock::now();
+    socketHdlr.updateMSG(sfield, t_value);
+    
+    if(socketHdlr.sendMSg()<0){
+        std::cerr << "unable to send message " << std::endl;
+        return -1;
+    }
+    end = std::chrono::steady_clock::now();
+    std::cout << "Update and send message (without checksum) 2: " << (double)std::chrono::duration_cast<std::chrono::microseconds>(end-start).count()/1000.0 << " ms" << std::endl;
+
+    // std::cout << "the sent message: " << socketHdlr.getFullmsg() << std::endl;
 
 
     socketHdlr.closeCommunication();
